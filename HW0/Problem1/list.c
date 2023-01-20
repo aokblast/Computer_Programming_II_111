d218d91952363b69763e1055a74bd64ea7ca3f41
aokblast <i19780219111@kimo.com>
2023-01-21 02:55:52 +0800

    init: add hw0-p1

---
+++ HW0/Problem1/list.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include "list.h"
+
+void clear() {
+
+}
+
+int32_t *insert(int32_t *loc, int32_t val) {
+  return NULL;
+}
+
+int32_t *erase(int32_t *loc) {
+  return NULL;
+}
+
+void push_back(int32_t val) {
+
+}
+
+void pop_back() {
+
+}
+
+void push_front(int32_t val) {
+
+}
+
+void pop_front() {
+
+}
+
+int32_t *find(int32_t val) {
+  return NULL;
+}
---
+++ HW0/Problem1/list.h
@@ -0,0 +1,67 @@
+#include <stdint.h>
+
+
+/**
+ * Clear the whole list
+ */
+
+void clear();
+
+/**
+ * Insert the specific elements into given location
+ * @param loc the pointer point to the location you want to insert
+ * @param val the value you want to insert
+ * @return pointer to the location you want to insert
+ */
+
+int32_t *insert(int32_t *loc, int32_t val);
+
+/**
+ * Erase the specific elements of given location
+ * @param loc the location of element you want to erase
+ * @return the location of element of same index after erasure.
+ */
+
+int32_t *erase(int32_t *loc);
+
+/**
+ * Push the element to the back of list
+ * @param val value you want to insert
+ */
+
+void push_back(int32_t val);
+
+/**
+ * Pop the element in the back of list
+ */
+
+void pop_back();
+
+/**
+ * Push the element to the front of list
+ * @param val value you want to insert
+ */
+
+void push_front(int32_t val);
+
+/**
+ * Pop the element in the front of list
+ */
+
+void pop_front();
+
+/**
+ * find the element of the given value
+ * @param val the value you want to find
+ * @return pointer of the value. If not found, return NULL
+ */
+
+int32_t *find(int32_t val);
+
+
+/**
+ * give the size of lsit
+ * @return return the size of list
+ */
+
+uint64_t size();
