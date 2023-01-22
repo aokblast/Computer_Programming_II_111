5acd94488c5c88c41d4f8fa01b4362dd429dbe8f
aokblast <i19780219111@kimo.com>
2023-01-22 21:48:25 +0800

    feat: change struct to array.

---
+++ HW0/auto/hw0.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "hw0"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "report"
+    "rep10"
+    "listings"
+    "hyperref"))
+ :latex)
+
